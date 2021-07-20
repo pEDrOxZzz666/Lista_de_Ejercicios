362b82fbb1910455aaeec29a4eb28dba570df9cc
Pedro <pedroxzzz@gmail.com>
2021-07-20 15:31:34 +0200

    Directiva 'ngif'

--- src/app/collection-ejercicios.ts
+++ src/app/collection-ejercicios.ts
@@ -6,6 +6,6 @@ export const EJERCICIOS: Ejercicio[] = [
   {id:3, name:'Curl Biceps'},
   {id:4, name:'Dominada'},
   {id:5, name:'Plancha'},
-  {id:6, name:'Press mancuerna hombro'}
+  {id:6, name:'Press mancuerna'}
 ]
 
